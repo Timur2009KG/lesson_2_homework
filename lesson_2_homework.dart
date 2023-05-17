603951ec2b7845f4a79e126bec436b33c503fff8
Timur2009KG <asanalievt93@gmail.com>
2023-05-17 21:01:19 +0300

    first commit

---
+++ lesson_2_homework.dart
@@ -0,0 +1,192 @@
+    void main() {
+      num a = 445;
+      num b = 44;
+      num c = 4;
+      num d = 5;
+      num e = 6;
+      print(a);
+      print(b);
+      print(c);
+      print(d);
+      print(e);
+
+
+      int f1 = 400;
+      int f2 = 399;
+      int f3 = 398;
+      int f4 = 397;
+      int f5 = 396;
+      print(f1);
+      print(f2);
+      print(f3);
+      print(f4);
+      print(f5);
+
+      double g1 = 500;
+      double g2 = 500;
+      double g3 = 500;
+      double g4 = 500;
+      double g5 = 500;
+      print(g1);
+      print(g2);
+      print(g3);
+      print(g4);
+      print(g5);
+
+
+      String h1 = 'Dastan';
+      String h2 = 'Samar';
+      String h3 = 'Chyngyz';
+      String h4 = 'Asan';
+      String h5 = 'Kenje';
+      print(h1);
+      print(h2);
+      print(h3);
+      print(h4);
+      print(h5);
+
+
+      List i1 = ['Tualet','Prachechnaya'];
+      List i2 =  ['jerar','Anwar'];
+      List i3 =  ['pervuy syn','gord'];
+      List i4 =  ['vtoroy syn','vysokomeren'];
+      List i5 =  ['tretiy syn','smiren'];
+        print(i1);
+        print(i2);
+        print(i3);
+        print(i4);
+        print(i5);
+
+      Map student1 = {
+        'name' : 'Artyr',
+        'age' : '20',
+        'isMan' : true,
+      };
+
+      Map student2 = {
+        'name' : 'Asan',
+        'age' : '30',
+        'isMan' : true,
+      };
+
+      Map student3 = {
+        'name' : 'Ayana',
+        'age' : '22',
+        'isMan' : false,
+      };
+
+      Map student4 = {
+        'name' : 'Sirena',
+        'age' : '20',
+        'isMan' : false,
+      };
+
+      Map student5 = {
+        'name' : 'Arman',
+        'age' : '20',
+        'isMan' : true,
+      };
+      print(student5);
+      print(student4);
+      print(student3);
+      print(student2);
+      print(student1);
+
+       
+    String runes1 = '\u{1F62F}';
+    String runes2 = '\u{1F61C}';
+    String runes3 = '\u{1F618}';
+    String runes4 = '\u{1F633}';
+    String runes5 = '\u{1F60D}';
+
+
+
+    print(runes1);
+    print(runes2);
+    print(runes3);
+    print(runes4);
+    print(runes5);
+
+    final lesson;
+    lesson = 5000;
+
+    final lesso;
+    lesso = 4000;
+
+    final less;
+    less = 3000;
+    
+    final les;
+    les = 2000;
+
+    final le;
+    le = 1000;
+
+
+    print(lesson);
+    print(lesso);
+    print(less);
+    print(les);
+    print(le);
+
+    const userName = 50;
+    const userServise = 40;
+    const userHome = 30;
+    const userVariant = 20;
+    const userNickname = 10;
+
+    print(userName);
+    print(userHome);
+    print(userServise);
+    print(userVariant);
+    print(userNickname);
+
+    dynamic myBoyfriend = 'myFriend';
+    myBoyfriend = ['Flutter'];
+    print(myBoyfriend);
+
+    dynamic mygirlfriend = 'myFriend';
+    print(mygirlfriend);
+
+    dynamic myBestHome = 'myHome';
+    print(myBestHome);
+
+    dynamic myMucbook = 'myLaptop';
+    print(myMucbook);
+
+    dynamic multigen = 'myGens';
+    print(multigen);
+
+    dynamic myBestGameMynecraft = 'myGame';
+    print(myBestGameMynecraft);
+
+
+    var bestGamer = 'best_people';
+    bestGamer = 'user';
+    print(bestGamer);
+
+    var input = 'ask';
+    input = 'Flutter';
+    print(input);
+
+    var dekstop = 'test_programm';
+    dekstop = 'myComputer';
+    print(dekstop);
+
+
+    var gamer = 'minecraft';
+    gamer = 'bestGame';
+    print(gamer);
+
+    var money = 10;
+    money = 50 ;
+    print(money);
+
+
+
+    
+
+
+
+
+    }
